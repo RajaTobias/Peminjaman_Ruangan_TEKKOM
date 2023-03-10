72d75736f9ba31b9e287b4ef441db07e8ab1ee20
myogaainurrofiq <m.yogaainurrofiq25@gmail.com>
2023-03-10 23:51:58 +0700

    update label

--- resources/views/User/statuspinjam.blade.php
+++ resources/views/User/statuspinjam.blade.php
@@ -48,9 +48,34 @@
                                 <td>{{ $data->Keperluan }}</td>
                                 <td>{{ $data->Ruangan }}</td>
                                 <td>{{ $data->Tanggal }}</td>
+
                                 <td>{{ $data->Jam_mulai }} s/d {{ $data->Jam_selesai }}</td>
-                                <td>{{ ($data->is_accept == 1 && $data->is_decline == 0 )? 'diterima' :  
-                                    (($data->is_accept == 0 && $data->is_decline == 1) ? 'ditolak' : 'menunggu')}}</td>
+                                <td>
+                                @if($data->is_accept == 1 && $data->is_decline == 0 )
+                                <div class="badge1">
+                                        <span class="teksbadge">
+                                        <i class="bx bx-no-check-circle icon"></i>  Diterima
+                                    </span>
+                                    </div>
+                                    @else
+                                        @if($data->is_accept == 0 && $data->is_decline == 1 )
+                                    <div class="badge2">
+                                        <span class="teksbadge">
+                                        <i class="bx bx-no-entry icon"></i>  Ditolak
+                                    </span>
+                                    </div>
+                                        @else
+                                    <div class="badge3">
+                                        <span class="teksbadge">
+                                        <i class="bx bx-time icon"></i>  Menunggu
+                                    </span>
+                                    </div>
+                                        @endif
+                                    @endif
+                                </td>
+                                    
+                                <!-- {{ ($data->is_accept == 1 && $data->is_decline == 0 )? 'diterima' :  
+                                    (($data->is_accept == 0 && $data->is_decline == 1) ? 'ditolak' : 'menunggu')}}</td> -->
                                 <!-- <td>
                                     <div class="pemohonBtn">
                                         <a href="" type="button" class="btn btn-success rounded-3">
