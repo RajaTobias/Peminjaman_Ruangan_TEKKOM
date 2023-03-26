d7904bbc2de5e72c783e0f14bd6459a8c0d4ec90
myogaainurrofiq <m.yogaainurrofiq25@gmail.com>
2023-03-27 00:45:24 +0700

    98%

--- resources/views/User/editprofile.blade.php
+++ resources/views/User/editprofile.blade.php
@@ -21,7 +21,7 @@
                 <img src="/assets/img/Vector (5).png" alt="..." allign="middle">
                 <div class="containerp">
                     <p class="teksp">Profile Setting</p>
-                    <form class="pprofile" method ="post" action = "{{route('User.updateprofile')}}">
+                    <form class="pprofile" method ="post" enctype="multipart/form-data" action = "{{route('User.updateprofile')}}">
                         @csrf
                         <div class="profile-pic">
                             <img src="{{ asset('assets/img/Rectangle 4036.png') }}" id="fotop">
