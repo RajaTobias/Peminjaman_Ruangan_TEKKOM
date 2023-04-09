ade3f189cc11a11fb2d0582bb32c4deca444ac1b
myogaainurrofiq <m.yogaainurrofiq25@gmail.com>
2023-04-10 00:01:57 +0700

    modal fix

--- resources/views/Admin/modal/import.blade.php
+++ resources/views/Admin/modal/import.blade.php
@@ -6,11 +6,17 @@
                     <h5 class="modal-title" style="color: #000;">Import Excel</h5>
                   </div>
                   <div class="modal-body">
-
-                  </div>
+                  <form action="" method="post" enctype="multipart/form-data">
+                  {{ csrf_field() }}
+                    <div class="form-group">
+                        <input type="file" name="file" required>
+                    </div>
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-success">Import</button>
                   </div>
+                  </div>
+                  </form>
                 </div>
               </div>
             </div>
